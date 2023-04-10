675b440cbf80ebea04da524bfd11bf4eb6a2e9c7
Quentin Gibson <quentingibson94@gmail.com>
2023-04-10 00:38:19 -0400

    handle adding user but not removing them

--- app/routes/_landing.messages.newmessage.tsx
+++ app/routes/_landing.messages.newmessage.tsx
@@ -1,298 +1,133 @@
+import { useLoaderData } from "@remix-run/react";
+import { LoaderArgs, json } from "@remix-run/server-runtime";
+import { useState } from "react";
+import Avatar from "react-avatar";
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+}
+export const loader = async ({ request, params }: LoaderArgs) => {
+
+  const data: User[] = [
+    {
+      id: 1,
+      name: "Taylor",
+      username: "@tails23",
+    },
+    {
+      id: 2,
+      name: "Kevin",
+      username: "@kevin27",
+    },
+    {
+      id: 3,
+      name: "Green",
+      username: "@green63",
+    },
+    {
+      id: 4,
+      name: "Dylan",
+      username: "@dylan89",
+    },
+    {
+      id: 5,
+      name: "Smith",
+      username: "@smith45",
+    },
+  ];
+
+  return json({ friendList: data })
+
+};
+
+
+
+
+
 export default function NewMessageRoute() {
-return (
-  <>
-          <div className="tt-title-content">
-            <a href="#" className="tt-toggle-aside">
-              <i className="tt-icon">
-                <svg className="icon">
-                  <use xlinkHref="#icon-arrow_left" />
-                </svg>
-              </i>
-            </a>
-            <h2 className="tt-title">New message</h2>
-          </div>
-          <div className="tt-search-compose">
-            <div className="tt-input">
-              <input
-                type="text"
-                className="tt-search-input"
-                placeholder="Type a name or multiple names"
-              />
+  const { friendList } = useLoaderData<typeof loader>();
+  const [visibleFriends, setVisibleFriends] = useState<User[]>(friendList);
+  const [friends, setFriends] = useState<User[]>([]);
+  function handleFriendSearch(e: React.ChangeEvent<HTMLInputElement>) {
+    const { value } = e.target;
+    setVisibleFriends(friendList.filter((friend) => {
+      return friend.name.toLowerCase().includes(value.toLowerCase());
+    }))
+  }
+  function handleFriendClick(_e: any, friendId: number) {
+    const friend = friendList.find((friend) => friend.id === friendId);
+    if (friend) {
+      setFriends([...friends, friend]);
+    }
+  }
+  return (
+    <>
+      <div className="tt-title-content">
+        <h2 className="tt-title">New message</h2>
+      </div>
+      <div className="tt-search-compose">
+        <div>
+          {friends.map((friend) => (
+            <div className="tt-item">
+              <Avatar name={friend.name} size="30" round={true} />
+              <span className="tt-name">{friend.name}</span>
             </div>
-            <div
-              className="tt-search-results ps-container ps-theme-default ps-active-y"
-              data-ps-id="fddd637f-fee1-46e3-9942-25514b97d18b"
-            >
-              <a href="#" className="tt-item">
-                <div className="tt-col-avatar">
-                  <svg className="tt-icon">
-                    <use xlinkHref="#icon-ava-t" />
-                  </svg>
-                </div>
-                <div className="tt-col-description">
-                  <h4 className="tt-title">
-                    <span>Taylor</span>
-                  </h4>
-                  <div className="tt-value">@tails23</div>
-                </div>
+          ))}
+        </div>
+        <div className="tt-input">
+          <input
+            type="text"
+            className="tt-search-input"
+            placeholder="Type a name or multiple names"
+            onChange={handleFriendSearch}
+          />
+        </div>
+        <div
+          className="tt-search-results ps-container ps-theme-default ps-active-y"
+          data-ps-id="fddd637f-fee1-46e3-9942-25514b97d18b"
+        >
+          {visibleFriends.map((friend) => (
+            <Friend friend={friend} handleFriendClick={handleFriendClick} />
+          ))}
+        </div>
+      </div>
+      <div className="tt-wrapper-inner">
+        <div className="pt-editor form-default">
+          <div className="form-group">
+            <textarea
+              name="message"
+              className="form-control"
+              rows={5}
+              placeholder="Write your message here"
+              defaultValue={""}
+            />
+          </div>
+          <div className="pt-row">
+            <div className="col-auto ml-auto">
+              <a href="#" className="btn btn-secondary btn-custom">
+                Send
               </a>
-              <a href="#" className="tt-item">
-                <div className="tt-col-avatar">
-                  <svg className="tt-icon">
-                    <use xlinkHref="#icon-ava-k" />
-                  </svg>
-                </div>
-                <div className="tt-col-description">
-                  <h4 className="tt-title">
-                    <span>Kevin</span>
-                  </h4>
-                  <div className="tt-value">@kevin27</div>
-                </div>
-              </a>
-              <a href="#" className="tt-item">
-                <div className="tt-col-avatar">
-                  <svg className="tt-icon">
-                    <use xlinkHref="#icon-ava-g" />
-                  </svg>
-                </div>
-                <div className="tt-col-description">
-                  <h4 className="tt-title">
-                    <span>Green</span>
-                  </h4>
-                  <div className="tt-value">@green63</div>
-                </div>
-              </a>
-              <a href="#" className="tt-item">
-                <div className="tt-col-avatar">
-                  <svg className="tt-icon">
-                    <use xlinkHref="#icon-ava-d" />
-                  </svg>
-                </div>
-                <div className="tt-col-description">
-                  <h4 className="tt-title">
-                    <span>Dylan</span>
-                  </h4>
-                  <div className="tt-value">@dylan89</div>
-                </div>
-              </a>
-              <a href="#" className="tt-item">
-                <div className="tt-col-avatar">
-                  <svg className="tt-icon">
-                    <use xlinkHref="#icon-ava-s" />
-                  </svg>
-                </div>
-                <div className="tt-col-description">
-                  <h4 className="tt-title">
-                    <span>Smith</span>
-                  </h4>
-                  <div className="tt-value">@smith45</div>
-                </div>
-              </a>
-              <a href="#" className="tt-item">
-                <div className="tt-col-avatar">
-                  <svg className="tt-icon">
-                    <use xlinkHref="#icon-ava-t" />
-                  </svg>
-                </div>
-                <div className="tt-col-description">
-                  <h4 className="tt-title">
-                    <span>Taylor</span>
-                  </h4>
-                  <div className="tt-value">@tails23</div>
-                </div>
-              </a>
-              <a href="#" className="tt-item">
-                <div className="tt-col-avatar">
-                  <svg className="tt-icon">
-                    <use xlinkHref="#icon-ava-k" />
-                  </svg>
-                </div>
-                <div className="tt-col-description">
-                  <h4 className="tt-title">
-                    <span>Kevin</span>
-                  </h4>
-                  <div className="tt-value">@kevin27</div>
-                </div>
-              </a>
-              <a href="#" className="tt-item">
-                <div className="tt-col-avatar">
-                  <svg className="tt-icon">
-                    <use xlinkHref="#icon-ava-g" />
-                  </svg>
-                </div>
-                <div className="tt-col-description">
-                  <h4 className="tt-title">
-                    <span>Green</span>
-                  </h4>
-                  <div className="tt-value">@green63</div>
-                </div>
-              </a>
-              <a href="#" className="tt-item">
-                <div className="tt-col-avatar">
-                  <svg className="tt-icon">
-                    <use xlinkHref="#icon-ava-d" />
-                  </svg>
-                </div>
-                <div className="tt-col-description">
-                  <h4 className="tt-title">
-                    <span>Dylan</span>
-                  </h4>
-                  <div className="tt-value">@dylan89</div>
-                </div>
-              </a>
-              <a href="#" className="tt-item">
-                <div className="tt-col-avatar">
-                  <svg className="tt-icon">
-                    <use xlinkHref="#icon-ava-s" />
-                  </svg>
-                </div>
-                <div className="tt-col-description">
-                  <h4 className="tt-title">
-                    <span>Smith</span>
-                  </h4>
-                  <div className="tt-value">@smith45</div>
-                </div>
-              </a>
-              <div
-                className="ps-scrollbar-x-rail"
-                style={{ left: 0, bottom: 3 }}
-              >
-                <div
-                  className="ps-scrollbar-x"
-                  tabIndex={0}
-                  style={{ left: 0, width: 0 }}
-                />
-              </div>
-              <div
-                className="ps-scrollbar-y-rail"
-                style={{ top: 0, height: 290, right: 3 }}
-              >
-                <div
-                  className="ps-scrollbar-y"
-                  tabIndex={0}
-                  style={{ top: 0, height: 150 }}
-                />
-              </div>
             </div>
           </div>
-          <div className="tt-wrapper-inner">
-            <div className="pt-editor form-default">
-              <div className="pt-row">
-                <div className="col-left">
-                  <ul className="pt-edit-btn">
-                    <li>
-                      <button type="button" className="btn-icon">
-                        <svg className="tt-icon">
-                          <use xlinkHref="#icon-quote" />
-                        </svg>
-                      </button>
-                    </li>
-                    <li>
-                      <button type="button" className="btn-icon">
-                        <svg className="tt-icon">
-                          <use xlinkHref="#icon-bold" />
-                        </svg>
-                      </button>
-                    </li>
-                    <li>
-                      <button type="button" className="btn-icon">
-                        <svg className="tt-icon">
-                          <use xlinkHref="#icon-italic" />
-                        </svg>
-                      </button>
-                    </li>
-                    <li>
-                      <button type="button" className="btn-icon">
-                        <svg className="tt-icon">
-                          <use xlinkHref="#icon-share_topic" />
-                        </svg>
-                      </button>
-                    </li>
-                    <li>
-                      <button type="button" className="btn-icon">
-                        <svg className="tt-icon">
-                          <use xlinkHref="#icon-blockquote" />
-                        </svg>
-                      </button>
-                    </li>
-                    <li>
-                      <button type="button" className="btn-icon">
-                        <svg className="tt-icon">
-                          <use xlinkHref="#icon-performatted" />
-                        </svg>
-                      </button>
-                    </li>
-                    <li className="hr" />
-                    <li>
-                      <button type="button" className="btn-icon">
-                        <svg className="tt-icon">
-                          <use xlinkHref="#icon-upload_files" />
-                        </svg>
-                      </button>
-                    </li>
-                    <li>
-                      <button type="button" className="btn-icon">
-                        <svg className="tt-icon">
-                          <use xlinkHref="#icon-bullet_list" />
-                        </svg>
-                      </button>
-                    </li>
-                    <li>
-                      <button type="button" className="btn-icon">
-                        <svg className="tt-icon">
-                          <use xlinkHref="#icon-heading" />
-                        </svg>
-                      </button>
-                    </li>
-                    <li>
-                      <button type="button" className="btn-icon">
-                        <svg className="tt-icon">
-                          <use xlinkHref="#icon-horizontal_line" />
-                        </svg>
-                      </button>
-                    </li>
-                    <li>
-                      <button type="button" className="btn-icon">
-                        <svg className="tt-icon">
-                          <use xlinkHref="#icon-emoticon" />
-                        </svg>
-                      </button>
-                    </li>
-                    <li>
-                      <button type="button" className="btn-icon">
-                        <svg className="tt-icon">
-                          <use xlinkHref="#icon-settings" />
-                        </svg>
-                      </button>
-                    </li>
-                    <li>
-                      <button type="button" className="btn-icon">
-                        <svg className="tt-icon">
-                          <use xlinkHref="#icon-color_picker" />
-                        </svg>
-                      </button>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div className="form-group">
-                <textarea
-                  name="message"
-                  className="form-control"
-                  rows={5}
-                  placeholder="Write your message here"
-                  defaultValue={""}
-                />
-              </div>
-              <div className="pt-row">
-                <div className="col-auto ml-auto">
-                  <a href="#" className="btn btn-secondary btn-custom">
-                    Send
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          </>
- )
+        </div>
+      </div>
+    </>
+  )
+}
+function Friend({ friend, handleFriendClick }: { friend: User, handleFriendClick: (e: any, friendId: number) => void }) {
+  return (
+    <button onClick={(e) => handleFriendClick(e, friend.id)} className="tt-item">
+      <div className="tt-col-avatar">
+        <Avatar className="tt-icon" name={friend.name} size="40" round={true} maxInitials={1} />
+      </div>
+      <div className="tt-col-description">
+        <h4 className="tt-title">
+          <span>{friend.name}</span>
+        </h4>
+        <div className="tt-value">{friend.username}</div>
+      </div>
+    </button>
+  );
 }
